669d515e11283a472cc3e15d3332ece13f3368c8
arcewp15 <970@holbertonschool.com>
2020-02-03 17:51:04 -0500

    [Fix] delete our swap function

--- sort.h
+++ sort.h
@@ -15,7 +15,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 void bubble_sort(int *array, size_t size);
-void _swap(int *number_1, int *number_2);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
