284bee3f4da7423e45a7db56f969a1e51a26b97c
arcewp15 <970@holbertonschool.com>
2020-02-03 18:00:29 -0500

    [Update] adding style of the code (Betty style)

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,4 +1,13 @@
 #include "sort.h"
+/**
+ * partition - implement the Lomuto partition scheme
+ * @arr: pointer to the array
+ * @low: initial position
+ * @high: last position
+ * @size: the lenght of array
+ *
+ * Return: the next position to validate into array
+ */
 int partition(int *arr, int low, int high, size_t size)
 {
 	int pivot = arr[high];
@@ -24,18 +33,33 @@ int partition(int *arr, int low, int high, size_t size)
 		print_array(arr, size);
 	return (i + 1);
 }
+/**
+ * sorting - recursive function to organize and validate each position of array
+ * @arr: pointer to the array
+ * @low: initial position
+ * @high: last position
+ * @size: the lenght of array
+ *
+ */
 void sorting(int *arr, int low, int high, size_t size)
 {
 	if (low < high)
 	{
 		int pi = partition(arr, low, high, size);
+
 		sorting(arr, low, pi - 1, size);
 		sorting(arr, pi + 1, high, size);
 	}
 }
-
+/**
+ * quick_sort - sorts an array of integers in ascending
+ * order using the Quick sort algorithm
+ * @array: pointer to the array
+ * @size: the lenght of the array
+ */
 void quick_sort(int *array, size_t size)
 {
 	int high = size - 1;
+
 	sorting(array, 0, high, size);
 }
