6fc9dfb0b3dfc14b94b262ea76a70e44bd77be80
MichaelAuditore <michael_v613@hotmail.com>
2020-02-03 17:09:07 +0000

    [Fix] Print only when it occurs a swap

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,25 +2,31 @@
 /**
  * selection_sort - sorts an array of integers in ascending order
  * using the Selection sort algorithm
- *
- *
+ * @array: Pointer to array to sort
+ * @size: Length of array to sort
+ * Return: Not Return
  */
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, less;
-	int tmp;
+	int tmp, count;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		less = i;
+		count = 0;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[less])
+			{
 				less = j;
+				count++;
+			}
 		}
 		tmp = array[less];
 		array[less] = array[i];
 		array[i] = tmp;
-		print_array(array, size);
+		if (count != 0)
+			print_array(array, size);
 	}
 }
